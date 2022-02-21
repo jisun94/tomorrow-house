2331f8d9932a482f44fb4ec3af3d62ce21b9a30b
jisun94 <jisun94lee@gmail.com>
2022-02-21 13:49:02 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,6 +1,10 @@
 const gnbSearch = document.querySelector('.gnb-search');
 const gnbSearchInput = gnbSearch.querySelector('input');
 const gnbSearchHistory = gnbSearch.querySelector('.search-history');
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol');
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+);
 
 function closeGnbSearchHistory(e) {
   if (!gnbSearch.contains(e.target)) {
@@ -10,10 +14,21 @@ function closeGnbSearchHistory(e) {
 }
 
 function openGnbSearchHistory() {
+  if (gnbSearchHistoryList.children.length === 0) {
+    return;
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistory);
   }
   gnbSearchHistory.classList.add('is-active');
 }
 
+function deleteAllSearchHistory() {
+  gnbSearchHistoryList.innerHTML = '';
+  gnbSearchHistory.classList.remove('is-active');
+}
+
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistory);
