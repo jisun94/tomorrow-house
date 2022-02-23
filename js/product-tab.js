746f752ba04f88cb0a24c313a1d33dc31f722c5e
jisun94 <jisun94lee@gmail.com>
2022-02-23 23:54:01 +0900

    💫 Scroll to tab-panel when produc-tab-item is clicked

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,18 +1,36 @@
 const productTab = document.querySelector('.product-tab');
 const productTabButtonList = productTab.querySelectorAll('button');
 
+const TOP_HEADER_DESKTOP = 80 + 50 + 54;
+const TOP_HEADER_MOBILE = 50 + 40 + 40;
+
 let currentActiveTab = productTab.querySelector('.is-active');
 
 function toggleActiveTab() {
   const tabItem = this.parentNode;
 
   if (currentActiveTab !== tabItem) {
+    tabItem.classList.add('is-active');
     currentActiveTab.classList.remove('is-active');
     currentActiveTab = tabItem;
   }
-  tabItem.classList.add('is-active');
+}
+
+function scrollToTabPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby');
+  const tabPanel = document.querySelector(`#${tabPanelId}`);
+
+  const scrollAmout =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE);
+
+  window.scrollBy({
+    top: scrollAmout,
+    behavior: 'smooth',
+  });
 }
 
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab);
+  button.addEventListener('click', scrollToTabPanel);
 });
